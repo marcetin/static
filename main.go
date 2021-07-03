c7557e6fa8efc35d3fe62fd2accf108cc238a46a
marcetin <marcetin@gmail.com>
2021-07-03 09:19:30 +0200

    Init

--- main.go
+++ main.go
@@ -32,30 +32,30 @@ func main() {
 	//r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("/home/gorun/okno/templates/"))))
 	//log.Fatal(http.ListenAndServe(":80", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 
-	r.Host("{sub}.{domain}.{tld}").PathPrefix("/").Handler(h(r))
+	r.Host("{sub}.{domain}.{tld}").PathPrefix("/").Handler(http.HandlerFunc(h))
 	log.Fatal(http.ListenAndServe(":80", nil))
 	//log.Fatal(http.ListenAndServeTLS("","",":80", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 	//log.Fatal(www.ListenAndServe())
 	//log.Fatal(wwwtls.ListenAndServeTLS("", ""))
 }
 
-func h(r *mux.Router) http.Handler {
+func h(w http.ResponseWriter, r *http.Request) {
 	//return (w http.ResponseWriter, r *http.Request){
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//return func(w http.ResponseWriter, r *http.Request) {
 
-		path = path + mux.Vars(r)["domain"] + "." + mux.Vars(r)["tld"]
-		if mux.Vars(r)["sub"] != "" {
-			path = path + mux.Vars(r)["sub"] + "." + mux.Vars(r)["domain"] + "." + mux.Vars(r)["tld"]
-			fmt.Println("p221:", path)
-		}
-		fmt.Println("p1:", path)
+	path = path + mux.Vars(r)["domain"] + "." + mux.Vars(r)["tld"]
+	if mux.Vars(r)["sub"] != "" {
+		path = path + mux.Vars(r)["sub"] + "." + mux.Vars(r)["domain"] + "." + mux.Vars(r)["tld"]
+		fmt.Println("p221:", path)
+	}
+	fmt.Println("p1:", path)
 
-		http.StripPrefix("/", http.FileServer(http.Dir(path)))
-		//})
-		//return handlers.CORS()(handlers.CompressHandler(interceptHandler(r, defaultErrorHandler)))
-		//return handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)
-		//return http.StripPrefix("/", http.FileServer(http.Dir(path)))
-	})
+	http.StripPrefix("/", http.FileServer(http.Dir(path)))
+	//})
+	//return handlers.CORS()(handlers.CompressHandler(interceptHandler(r, defaultErrorHandler)))
+	//return handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)
+	//return http.StripPrefix("/", http.FileServer(http.Dir(path)))
+	//})
 }
 
 func defaultErrorHandler(w http.ResponseWriter, status int) {
